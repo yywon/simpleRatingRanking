b7b5a02eb564477b39ecbf40486052ad8a9ece4e
Ryan Kemmer (Student) <rwkemmer@sundevils.asu.edu>
2019-10-24 08:12:36 -0700

    editing dot generator

--- dotgenerator.py
+++ dotgenerator.py
@@ -10,82 +10,69 @@ area = np.pi*3
 
 masterArray = []
 
-noiseLevels = {1,2,4,8,16,32,64,128}
+PATH = 'C:/Users/rwkemmer/Desktop/dots/'
 
-#loop through noise levels and generate pictures
-for i in noiseLevels:
+pictureData = []
 
-    #PATH = 'C:/Users/rwkemmer/Desktop/dots/'
+os.chdir(PATH)
 
-    print("Noise Level: " + str(i))
+#each picture per noise level
+for j in range(40):
+    N = Base + j
 
-    pictureData = []
+    pictureData.append(N)
 
-    #os.chdir(PATH)
-    #os.mkdir(str(i))
+    existing_locations = []
+    existing_locations.append(np.array([0,0]))
+    dots = 0
 
-    #each picture per noise level
-    for j in range(4):
-        N = Base + (i * j)
+    #add each individual dot
+    while (dots < N):
 
-        pictureData.append(N)
+        x = np.random.rand()
+        y = np.random.rand()
 
-        existing_locations = []
-        existing_locations.append(np.array([0,0]))
-        dots = 0
+        new_location = np.array([x,y])
+        valid_checks = 0
 
-        #add each individual dot
-        while (dots < N):
-
-            x = np.random.rand()
-            y = np.random.rand()
-
-            new_location = np.array([x,y])
-            valid_checks = 0
-
-            #check to ensure new dot is not too close to others
-            for existing_location in existing_locations:
-                vecdif = existing_location - new_location
+        #check to ensure new dot is not too close to others
+        for existing_location in existing_locations:
+            vecdif = existing_location - new_location
                 
-                if ((abs(vecdif[0]) < .03) & (abs(vecdif[1]) < .03)):
-                    #print("broke criteria")
-                    break
-                else:
-                    valid_checks += 1
+            if ((abs(vecdif[0]) < .03) & (abs(vecdif[1]) < .03)):
+                #print("broke criteria")
+                break
+            else:
+                valid_checks += 1
 
-            if valid_checks == len(existing_locations):
-                dots += 1
-                existing_locations.append(new_location)    
-                #plt.plot(x, y, 'ko')
+        if valid_checks == len(existing_locations):
+            dots += 1
+            existing_locations.append(new_location)    
+            plt.plot(x, y, 'ko')
 
-        print("number of dots: " + str(dots))
+        
+    axes = plt.gca()
+    axes.set_xlim([-0.01,1.01])
+    axes.set_ylim([-0.01,1.01])
 
-        '''
-        axes = plt.gca()
-        axes.set_xlim([-0.01,1.01])
-        axes.set_ylim([-0.01,1.01])
+    plt.axes().set_aspect('equal')
+    plt.axis('off')
 
-        plt.axes().set_aspect('equal')
-        plt.axis('off')
+    plt.savefig(PATH + '/' + str(N),bbox_inches='tight', pad_inches=0)
 
-        plt.savefig(PATH + str(i) + '/' + str(N),bbox_inches='tight', pad_inches=0)
-
-        plt.clf()
-
-        '''
-
-    activityArray = [i, pictureData]
-
-    masterArray.append(activityArray)
+    plt.clf()
 
 permutationArray = []
 
-for i in masterArray:
+#change path to app template
+PATH = 'C:/Users/rwkemmer/Documents/apptemplate/'
+os.chdir(PATH)
+
     permutationsofActivity = []
     activity = i[0]
     pictureData = i[1]
     #print(pictureData)
-    permutationsofActivity = permutations(pictureData)
+    permutationsofActivity = permutations(pictureData, 4)
     permy = []
 
     counter = 0
@@ -109,27 +96,3 @@ for i in permutationArray:
 
 with open('public/data/questions.json', 'w') as outfile:
     json.dump(data, outfile)
-
-
-'''
-for i in permutationArray: 
-    ordering = i[2]
-    data.append({
-        'noiselevel': i[0],
-        'variation': i[1],
-        'array': ordering,
-        #'pos0': ordering[0],
-        #'pos1': ordering[1],
-        #'pos2': ordering[2],
-        #'pos3': ordering[3]
-    })
-
-#dump to outfile
-with open('public/data/questions.json', 'w') as outfile:
-    json.dump(data, outfile)
-
-'''
-
-
-
-
