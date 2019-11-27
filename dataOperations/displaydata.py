7af46d2fb85bf38d2047a553c5c47a017c9009e5
Ryan Kemmer (Student) <rwkemmer@sundevils.asu.edu>
2019-11-27 13:20:47 -0700

    updated data analysis

--- dataOperations/displaydata.py
+++ dataOperations/displaydata.py
@@ -1,6 +1,6 @@
 import json
 
-gtruth = [128,64,32,16,8,4,2,1]
+gtruth = [50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82]
 
 with open("responseData.json", "r") as read_file:
     data = json.load(read_file)
@@ -14,25 +14,29 @@ count = 0
 for response in data:
     groundtruth = response["groundtruth"]
 
-    print(response["rank_dif"])
+    #get counts for usability scores
+    if None not in(response["rank_dif"],response["rate_dif"],response["rank_ui"],response["rate_ui"]):
+        rank_dif += int(response["rank_dif"])
+        rate_dif += int(response["rate_dif"])
+        rank_ui += int(response["rank_ui"])
+        rate_ui += int(response["rate_ui"])
 
+        count += 1
     
-    rank_dif += int(response["rank_dif"])
-    rate_dif += int(response["rate_dif"])
-    rank_ui += int(response["rank_ui"])
-    rate_ui += int(response["rate_ui"])
-
-    count += 1
+    for picture in gtruth:
 
 rank_dif = rank_dif/count
-rate_dif += rate_dif
-rank_ui += rank_ui
-rate_ui += rate_ui
+rate_dif = rate_dif/count
+rank_ui = rank_ui/count
+rate_ui = rate_ui/count
 
-print("rank_dif" + rank_dif)
-print("rate_dif" + rate_dif)
-print("rank_ui" + rank_ui)
-print("rate_ui" + rate_ui)
+
+
+print("count " + str(count))
+print("rank_dif " + str(rank_dif))
+print("rate_dif " + str(rate_dif))
+print("rank_ui " + str(rank_ui))
+print("rate_ui " + str(rate_ui))
 
     
 
