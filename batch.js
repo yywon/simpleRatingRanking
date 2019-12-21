fadf5c17dc289600717c7effcfa35797a71a7385
Ryan Kemmer (Student) <rwkemmer@sundevils.asu.edu>
2019-12-20 17:37:03 -0700

    updated batch.js

--- batch.js
+++ batch.js
@@ -1,12 +1,32 @@
 module.exports = class batch {
+    
     constructor(size){
-        this.size = 0
-        this.complete = false
-        this.users = []
+        this.size = size
+        this.completeness = 60/size
+        this.assignmentStatus = [0]*this.completeness
+        this.questions = this.assignQuestions
     }
 
-    addUser(User){
-        this.users.push(User)
-    }
+    assignQuestions(){
 
+        let questions
+        let allNumbers = [];
+        for(i = 0; i < 60; i++){
+            allNumbers[i] = 50 + i
+        }
+
+        shuffle(allNumbers)
+
+        chunk = this.size
+        i = 0;
+        j = allNumbers.length; 
+
+        while(i < j){
+            temparray = allNumbers.slice(i,i+chunk);
+            questions.push(temparray)
+            i += chunk
+        }
+
+        return questions
+    }
 }
