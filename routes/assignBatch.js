b35fb5dcab0e7d82a95ed388a757538458fded6f
Ryan Kemmer (Student) <rwkemmer@sundevils.asu.edu>
2020-02-28 18:02:39 -0700

    updated

--- routes/assignBatch.js
+++ routes/assignBatch.js
@@ -8,7 +8,7 @@ const co = require('co');
 const User = require('../User');
 const Batch = require('../batch');
 
-var dbase = 'ratingsrankingsA2'
+var dbase = 'ratingsrankingsA3'
 
 const assignModule = {
 
