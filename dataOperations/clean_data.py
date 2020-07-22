e1a4b30a1732983da03b4f4b22e55762e09e7ea3
Ryan Kemmer <ryankemmer@Ryans-MacBook-Pro-8.local>
2020-07-22 13:08:31 -0700

    use this one

--- dataOperations/clean_data.py
+++ dataOperations/clean_data.py
@@ -1,3 +1,5 @@
+#script to only keep responses that are valid and tied to a user, as well as clear out users that have not finished
+
 import pymongo
 import json
 import sys
@@ -11,7 +13,7 @@ dbase = sys.argv[1]
 print(dbase)
 
 client = pymongo.MongoClient(url)
-db = client['ratingsrankingsframes']
+db = client[dbase]
 usersCol = db['users']
 responsesCol = db['responses']
 batchesCol = db['batches']
@@ -23,7 +25,6 @@ userRemove = 0
 args = len(sys.argv) - 1
 
 for user in usersCol.find():
-
     key2pay = user["key2pay"]
     userName = user["user"]
     indexes = user["indexes"]
@@ -35,36 +36,85 @@ for user in usersCol.find():
 
     print(userName + ": " + str(responseCount) + " responses. Key2pay: " + key2pay)
     
-    if(responseCount >= 26):
+    discard = 0
+    #check if indexes are in valid range
+    for i in range(4):
+        if indexes[i][0] == 2:
+            if indexes[i][1] > 3:
+			    discard = 1
+        if indexes[i][0] == 3:
+		    if indexes[i][1] > 5:
+			    discard = 1
+        if indexes[i][0] == 5:
+		    if indexes[i][1] > 9:
+			    discard = 1
+        if indexes[i][0] == 6:
+		    if indexes[i][1] > 11:
+			discard = 1
+
+    #validate user responses
+    for i in range(len(indexes)):
+
+        question = str(indexes[i][2])
+        update = {"$set": {}}
+        update['$set']["assignmentStatus."+question] = 2
+
+        batchesCol.update_many({'size': indexes[i][0], 'number': indexes[i][1]}, update)
+
+
+    if(responseCount >= 8 and discard == 0):
         completed_users.append(userName)
+
     else:
         userRemove += 1
-        if(args > 0):
-            if(sys.argv[1] == "delete"):
+        if(args > 1):
+            if(sys.argv[2] == "delete"):
 
-                #remove assignment from batch A
-                for i in range(len(indexesA)):
+                #remove assignment from batch 
+                for i in range(len(indexes)):
 
-                    question = str(indexesA[i][2])
+                    question = str(indexes[i][2])
                     update = {"$set": {}}
                     update['$set']["assignmentStatus."+question] = 0
 
-                    batchesColA.update_one({'size': indexesA[i][0], 'number': indexesA[i][1]}, update)
-
-                #remove assignment from batch B
-                for i in range(len(indexesB)):
-
-                    question = str(indexesB[i][2])
-                    update = {"$set": {}}
-                    update['$set']["assignmentStatus."+question] = 0
-
-                    batchesColB.update_one({'size': indexesB[i][0], 'number': indexesB[i][1]}, update)
+                    batchesCol.update_one({'size': indexes[i][0], 'number': indexes[i][1]}, update)
                 
                 responsesCol.delete_many({'user' : userName})
                 usersCol.delete_one({'user' : userName})
 
+
+#update all batches that have actually been assigned to 1, and those that havent to 0
+real = 0
+changed = 0
+ 
+for batch in batchesCol.find():
+
+    size = batch['size']
+    number = batch['number']
+    status = batch['assignmentStatus']
+
+    for i in range(len(status)):
+
+        assignment = str(status[i])
+
+        if assignment is "2":
+            real += 1
+
+            update = {"$set": {}}
+            update['$set']["assignmentStatus."+str(i)] = 1
+
+            batchesCol.update_many({'size': size, 'number': number}, update)
+
+        else:
+
+            changed += 1
+
+            update = {"$set": {}}
+            update['$set']["assignmentStatus."+str(i)] = 0
+
+            batchesCol.update_many({'size': size, 'number': number}, update)
+
+
 print("Completed Users: " + str(len(completed_users)))
 print("Users Removed: " + str(userRemove))
-
-
-
+print("Number of Valid Users: ", str(real/4))
